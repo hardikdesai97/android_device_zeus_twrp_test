01e5feab5797581cb151b0c427877a7af6619b2c
hardikdesai97 <53992875+hardikdesai97@users.noreply.github.com>
2024-12-06 20:16:38 +0530

    Update twrp_zeus.mk

--- twrp_zeus.mk
+++ twrp_zeus.mk
@@ -1,36 +1,45 @@
 #
-# Copyright 2018 The Android Open Source Project
-# Copyright 2014-2022 The Team Win LLC
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-# Define hardware platform
-PRODUCT_PLATFORM := taro
+DEVICE_PATH := device/xiaomi/zeus
 
-# The below variables will be generated automatically
-#
-# Release name (automatically taken from this file's suffix)
-PRODUCT_RELEASE_NAME := $(lastword $(subst /, ,$(lastword $(subst _, ,$(firstword $(subst ., ,$(MAKEFILE_LIST)))))))
-
-# Custom vendor used in build tree (automatically taken from this file's prefix)
-CUSTOM_VENDOR := $(lastword $(subst /, ,$(firstword $(subst _, ,$(firstword $(MAKEFILE_LIST))))))
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/$(CUSTOM_VENDOR)/config/common.mk)
-
-# OEM Info (automatically taken from device tree path)
-BOARD_VENDOR := $(or $(word 2,$(subst /, ,$(firstword $(MAKEFILE_LIST)))),$(value 2))
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
-PRODUCT_NAME := $(CUSTOM_VENDOR)_$(PRODUCT_RELEASE_NAME)
-PRODUCT_BRAND := $(BOARD_VENDOR)
-PRODUCT_MODEL := Xiaomi 12 Pro
-PRODUCT_MANUFACTURER := $(BOARD_VENDOR)
-
-# Device path for OEM device tree
-DEVICE_PATH := device/$(BOARD_VENDOR)/$(PRODUCT_DEVICE)
-
-# Inherit from hardware-specific part of the product configuration
+# Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := zeus
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := zeus
+PRODUCT_NAME := twrp_zeus
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi 12 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := $(PRODUCT_RELEASE_NAME)
+
+# Kernel modules
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko fts_touch_spi.ko"
+
+# Haptic
+TW_SUPPORT_INPUT_AIDL_HAPTICS := true
+TW_SUPPORT_INPUT_AIDL_HAPTICS_FQNAME := "IVibrator/vibratorfeature"
+
+# Theme
+TW_STATUS_ICONS_ALIGN := center
+TW_Y_OFFSET := 106
+TW_H_OFFSET := -106
