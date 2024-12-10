dd391430f33d07ae6c823f53c98f5076a7332bcb
hardikdesai97 <53992875+hardikdesai97@users.noreply.github.com>
2024-12-10 16:22:01 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -33,9 +33,14 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl.recovery
 
 PRODUCT_PACKAGES += \
-    bootctrl.xiaomi_sm8475 \
-    bootctrl.xiaomi_sm8475.recovery \
+    bootctrl.xiaomi_sm8450 \
+    bootctrl.xiaomi_sm8450.recovery \
     android.hardware.boot@1.2-impl-qti.recovery
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+	$(DEVICE_PATH) \
+	vendor/qcom/opensource/commonsys-intf/display
 
 # SHIPPING API
 PRODUCT_SHIPPING_API_LEVEL := 31
